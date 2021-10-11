d074809e8bcc378a03e238e75d90b1d7f501cf58
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-10-11 17:26:08 +0100

    first commit

---
+++ src/test/java/com/ecommerce/shop/ShopApplicationTests.java
@@ -0,0 +1,36 @@
+package com.ecommerce.shop;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest
+@Slf4j
+class ShopApplicationTests {
+
+    @Autowired
+	DataSource dataSource;
+
+    @Test
+    public void connectToDatabaseTest() {
+        assertThat(dataSource).isNotNull();
+        log.info("Datasource properties -> {}", dataSource);
+
+       try {
+           Connection connection = dataSource.getConnection();
+           assertThat(connection).isNotNull();
+           log.info("Database -> {}", connection.getCatalog());
+       }catch (SQLException exception) {
+           log.info("An exception occurred -> {}");
+       }
+    }
+}
