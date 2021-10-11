7e56f68088f28c4152bb020c1143267ea4ec8d3b
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-10-11 18:02:55 +0100

    update

--- src/test/java/com/ecommerce/shop/ShopApplicationTests.java
+++ src/test/java/com/ecommerce/shop/ShopApplicationTests.java
@@ -4,6 +4,7 @@ package com.ecommerce.shop;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.sql.DataSource;
@@ -20,6 +21,19 @@ class ShopApplicationTests {
     @Autowired
 	DataSource dataSource;
 
+    @Value("${test-name}")
+    private String name;
+    @Value("${test-year}")
+    private String currentYear;
+
+    @Test
+    public void printValues() {
+        assertThat(name).isEqualTo("Jack");
+        assertThat(currentYear).isEqualTo("2020");
+        log.info("Test name -> {}", name);
+        log.info("Test year -> {}", currentYear);
+    }
+
     @Test
     public void connectToDatabaseTest() {
         assertThat(dataSource).isNotNull();
@@ -30,7 +44,7 @@ class ShopApplicationTests {
            assertThat(connection).isNotNull();
            log.info("Database -> {}", connection.getCatalog());
        }catch (SQLException exception) {
-           log.info("An exception occurred -> {}");
+           log.info("An exception occurred -> {}", exception.getMessage());
        }
     }
 }
