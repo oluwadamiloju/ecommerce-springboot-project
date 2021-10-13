d3adbabcdcd361ae861829604abc2c4681f09b80
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-10-13 13:18:03 +0100

    updates

--- src/main/java/com/ecommerce/shop/web/controller/HomeController.java
+++ src/main/java/com/ecommerce/shop/web/controller/HomeController.java
@@ -2,13 +2,36 @@ package com.ecommerce.shop.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
+    @GetMapping("/index")
+    public String getIndex(){
+        return "index";
+    }
 
-    @GetMapping("/say-hello")
-    public @ResponseBody String getWelcomeMessage(){
-        return "Welcome to my first spring application!";
+    @GetMapping("/about")
+    public String getAbout(){
+        return "about";
+    }
+
+    @GetMapping("/contact")
+    public String getContact(){
+        return "contact";
+    }
+
+    @GetMapping("/products")
+    public String getProduct(){
+        return "product";
+    }
+
+    @GetMapping("/services")
+    public String getServices(){
+        return "services";
+    }
+
+    @GetMapping("/single")
+    public String getSingle(){
+        return "single";
     }
 }
