3d0f8a7b05c2fd456b518c0d2ca1ca627a43297e
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-10-25 14:28:54 +0100

    update

--- src/main/java/com/ecommerce/shop/web/controller/HomeController.java
+++ src/main/java/com/ecommerce/shop/web/controller/HomeController.java
@@ -14,7 +14,7 @@ public class HomeController {
     @Autowired
     ProductService productServiceImpl;
 
-    @GetMapping("/index")
+    @GetMapping(value = {"/", "/index"})
     public String getIndex(Model model){
         List<Product> productList = productServiceImpl.findAll();
         model.addAttribute("products", productList);
